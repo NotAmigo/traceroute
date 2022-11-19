f072f4e9ef06f66537cd36515d0e069fc92cbfcc
Ilya Kovalchuk <sckremyulock@gmail.com>
2022-11-19 20:16:00 +0500

    Added TCP and somne minor fixes

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 # Дополнительная часть:
 # Поддержка IPv6.
-# Работа через TCP SYN + указание порта.
 # Разрешать хопы в DNS-имя.
 # debug-режим.
 from scapy.all import *
 import time
 from typing import List, Tuple, Optional
-from collections import namedtuple
-
-# TODO: Want to rewrite tuple to namedtuple ^^^
+import click
 
 Answer = Tuple[Optional[str], List[Optional[int]]]  # TODO: python 3.10 change to | also is there better place to do it
 
@@ -16,24 +13,38 @@ hostname = "8.8.8.8"
 NETWORK_ROUTER_ADMIN_LOGIN = '192.168.1.120'
 
 
-class ICMPPacket:
-    def __init__(self, dst, ttl=1, id=RandShort(), seq=0, length=40, payload=None):
+class Packet:
+    def __init__(self, dst, id, seq, src):
         self.id = id
         self.seq = seq
-        self.payload = payload if payload is not None else str(RandString(length - 8))
-        self.ttl = ttl
         self.dst = dst
+        self.src = src
+
+    def get_packet(self, ttl):
+        raise NotImplementedError
+
+
+class ICMPPacket(Packet):
+    def __init__(self, dst, id: int, seq: int, src: str, length: int, payload):
+        super(ICMPPacket, self).__init__(dst, id, seq, src)
+        self.payload = payload if payload is not None else str(RandString(length - 8))
         self.length = length
 
-    def get_packet(self) -> IP:
-        return IP(dst=self.dst, ttl=self.ttl) / ICMP(id=self.id, seq=self.seq) / self.payload
-
-    def get_ping_packet(self, ttl) -> IP:
+    def get_packet(self, ttl) -> IP:
         return IP(dst=self.dst, ttl=ttl) / ICMP(id=self.id, seq=self.seq) / self.payload
 
 
+class TCPPacket(Packet):
+    def __init__(self, dst, id: int, seq: int, src: str, port: int):
+        super(TCPPacket, self).__init__(dst, id, seq, src)
+        self.port = port
+
+    def get_packet(self, ttl) -> TCP:
+        return IP(dst=self.dst, ttl=ttl) / TCP(flags=0x2, dport=self.port, sport=RandShort())
+
+
 class Ping:
-    def __init__(self, packet: IP, timeout: int, repeat: int, interval: int):
+    def __init__(self, packet: Packet, timeout: int, repeat: int, interval: int):
         self.timeout = timeout
         self.packet = packet
         self.repeat = repeat
@@ -44,7 +55,7 @@ class Ping:
         address = None
         for i in range(self.repeat):
             response = sr1(self.packet, timeout=self.timeout, verbose=0)  # TODO: convert ticks to ms
-            timestamp = round((response.time - self.packet.sent_time)*1000) if response is not None else None
+            timestamp = round((response.time - self.packet.sent_time) * 1000) if response is not None else None
             addr = response.src if response is not None else None
             if addr is not None:
                 address = addr
@@ -63,10 +74,9 @@ class AnswerFormatter:
 
 
 class Traceroute:
-    def __init__(self, dst: str, packet: ICMPPacket, src: str = NETWORK_ROUTER_ADMIN_LOGIN,
-                 max_ttl: int = 30, repeat: int = 3, timeout: int = 3, interval: int = 0):
+    def __init__(self, dst: str, packet: Packet,
+                 max_ttl: int, repeat: int, timeout: int, interval: int):
         self.dst = dst
-        self.src = src
         self.packet = packet
         self.interval = interval
         self.max_ttl = max_ttl
@@ -75,11 +85,37 @@ class Traceroute:
 
     def run(self):
         for ttl in range(1, self.max_ttl + 1):
-            ttl_packet = self.packet.get_ping_packet(ttl)
+            ttl_packet = self.packet.get_packet(ttl)
             ping = Ping(ttl_packet, self.timeout, self.repeat, self.interval)
             answer = ping.make_ping()
             print(f"{str(ttl).ljust(6)} {AnswerFormatter(answer)}")
+            if answer[0] == self.dst:
+                break
 
 
-traceroute = Traceroute(hostname, ICMPPacket(hostname))
-traceroute.run()
+@click.command()
+@click.option('--type', prompt='Type TCP or ICMP', help='Choose, ICMP or TCP traceroute')
+@click.option('--destination', prompt='Traceroute to', help='Destination IP address')
+@click.option('--source', default=NETWORK_ROUTER_ADMIN_LOGIN, help='Source IP address')
+@click.option('--id', default=0, help='Custom id')
+@click.option('--seq', default=0, help='Custom SEQ')
+@click.option('--len', default=40, help='Length of the ICMP packet. '
+                                        'If you type custom, payload, length will be ignored, length ignored for TCP')
+@click.option('--payload', default=None, help='Custom ICMP packet payload')
+@click.option('--tcp_port', default=80, help='Custom TCP destination port')
+@click.option('--max_ttl', default=30, help='Max TTL')
+@click.option('--repeat', default=3, help='Requests per TTL')
+@click.option('--timeout', default=3, help='Timeout for each request')
+@click.option('--interval', default=0, help='Interval between requests')
+def main(type, destination, source, id, seq, len, payload, tcp_port, max_ttl, repeat, timeout, interval):
+    if type == 'ICMP':
+        packet = ICMPPacket(destination, id, seq, source, len, payload)
+    elif type == 'TCP':
+        packet = TCPPacket(destination, id, seq, source, tcp_port)
+    else:
+        print('Wrong type')
+        return
+    traceroute = Traceroute(destination, packet, max_ttl, repeat, timeout, interval)
+    traceroute.run()
+
+main()
